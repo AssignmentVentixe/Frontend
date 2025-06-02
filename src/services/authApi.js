b930bcf13eceac6810a4cbfc49a6bf08ab44f484
Joakim Erik Åkerman <joakim.akerman@hotmail.com>
2025-06-02 17:37:10 +0200

    updated time

--- src/services/authApi.js
+++ src/services/authApi.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "https://auth-api-ventixe-b9dyccgkh4egdtd8.swedencentral-01.azurewebsites.net/api",
   withCredentials: true,
-  timeout: 10000,
+  timeout: 30000,
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
